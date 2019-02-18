871aae191b61482c5e621ec0d94fe51df99d38a1
Szymon Piecuch <szymek.piecuch@gmail.com>
2019-02-18 18:50:50 +0100

    Upgraded store component

--- src/store/components/store.component.js
+++ src/store/components/store.component.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import { createReduxStore } from '../store';
 
-class Store extends Component {
-  render() {
-    const { children, reducers } = this.props;
-    const store = createReduxStore(reducers);
+const Store = ({ children, reducers }) => {
+  const store = createReduxStore(reducers);
 
-    return <Provider store={store}>{children}</Provider>;
-  }
-}
+  return <Provider store={store}>{children}</Provider>;
+};
 
 Store.propTypes = {
   reducers: PropTypes.arrayOf(PropTypes.number).isRequired,
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
 };
 
-export default Store;
+export { Store };
